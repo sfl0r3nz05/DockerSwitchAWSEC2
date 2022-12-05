96fbddac421b6937c297ca6c5fd142cbb1ae887f
Ubuntu <ubuntu@ip-172-31-85-246.ec2.internal>
2022-12-05 08:47:33 +0000

    update

--- api-ec2/api/main.py
+++ api-ec2/api/main.py
@@ -15,4 +15,3 @@ def process_json(id):
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
-
--- api-ec2/api/status_ec2.py
+++ api-ec2/api/status_ec2.py
@@ -10,10 +10,6 @@ import boto3.ec2
 from botocore.exceptions import ClientError
 from flask import Flask, jsonify, request, json
 
-#aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), #Recover access key from env variables
-#aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"), #Recover secret access key from env variables
-#aws_session_token=os.environ.get("AWS_SESSION_TOKEN"), #Recover session token from env variables
-
 #def status(request):
 def status(id):
     """
@@ -22,31 +18,14 @@ def status(id):
     Try to get the EC2 instance status.
     """
     
-    #try:
-    #    region_name=os.environ.get("REGION_NAME")  #Recover region name from env variables
-    #    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), #Recover access key from env variables
-    #    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"), #Recover secret access key from env variables
-    #    aws_session_token=os.environ.get("AWS_SESSION_TOKEN"), #Recover session token from env variables
-    #    ec2 = boto3.client('ec2', region_name=region_name, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, aws_session_token=aws_session_token)
-    #    response = ec2.describe_instances()
-    #    print(response)
-    #    logging.info(response)
-    #    if (response):
-    #        return response
-    #    else:
-    #        resp = {'message': 'fail'}
-    #        return jsonify(resp)
-    #except ClientError as e:
-    #    print(e)
-
-    region_name=os.environ.get("REGION_NAME")  #Recover region name from env variables
-    aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"), #Recover access key from env variables
-    aws_secret_access_key=os.environ.get("AWS_SECRET_ACCESS_KEY"), #Recover secret access key from env variables
-    aws_session_token=os.environ.get("AWS_SESSION_TOKEN"), #Recover session token from env variables
-    ec2 = boto3.client('ec2', region_name=region_name, aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key, aws_session_token=aws_session_token)
-    if (ec2):
-        return ec2
-    else:
-        resp = {'message': 'fail'}
-        return jsonify(resp)
-
+    try:
+        region_name_var=os.environ.get("REGION_NAME")  #Recover region name from env variables
+        ec2 = boto3.client('ec2', region_name=region_name_var)
+        response = ec2.describe_instance_status(InstanceIds=[id])
+        if (response):
+            return jsonify(response)
+        else:
+            resp = {'message': 'fail'}
+            return jsonify(resp)
+    except ClientError as e:
+        print(e)
